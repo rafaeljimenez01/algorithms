bb7de8068d5354c2f55552ac3a983e42d6d7e674
rafaeljimenez01 <rafaeljimenez_01@hotmail.com>
2020-11-18 16:32:45 -0500

    Checks if a string is a step away from other string

---
+++ oneAway.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/*Check if two strings are one or zero edits away
+    Edits:
+        insert a char
+        remove a char
+        replace a char
+
+*/
+int main(){
+
+
+
+
+
+    return 0;
+}

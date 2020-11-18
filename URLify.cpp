3337e48388876fcc8b5e1b2b388598384f327f46
rafaeljimenez01 <rafaeljimenez_01@hotmail.com>
2020-11-17 20:19:23 -0500

    URLify

---
+++ URLify.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+const int MAX = 100;
+
+void URLify(char s[]){
+    int spaces = 0;
+    for(int i=0; s[i]; i++) {
+        if(s[i]== ' ')
+            spaces++;
+    }   
+
+
+
+};
+int main(){
+    char str[MAX] = "Mr John Smith    "
+
+
+
+
+    return 0;
+}

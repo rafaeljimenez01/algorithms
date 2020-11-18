f43c5ea93c60366fa6a555e929cf9f81155322fe
rafaeljimenez01 <rafaeljimenez_01@hotmail.com>
2020-11-17 23:24:30 -0500

    palindromePermutation problem, working with spaces

--- palindromePermutation.cpp
+++ palindromePermutation.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
-
+#include <unordered_map>
+#include <string>
 using namespace std;
+//106 121 134 136
 
+//tact coa
+
+//         taco cat  
+//backward:taco cat 
+//even number of each char(uneven length only one letter to occurr odd number of times)
+
+bool palindromePermutation(string& str){
+    unordered_map<char,int> umap;
+    //remove all spaces to make algo easier 
+    int noSpaces = 0;
+    for(int i=0;i < str.size();i++){
+        if(str[i] != ' ')
+            str[noSpaces++] = str[i];
+    }
+    str[noSpaces] = '\0';
+
+    for(int i=0; str[i]; i++){
+        umap[str[i]]++;
+    }
+    int count =0;
+    for(auto& itr: umap){
+        if(itr.second % 2 != 0)
+            count++;
+    }
+    if(count <=1)
+        return true;
+    else
+        return false;
+    
+};
 
 int main(){
-
+    string str = "cec            arar";
+    cout << palindromePermutation(str) << endl;
 
 
 
